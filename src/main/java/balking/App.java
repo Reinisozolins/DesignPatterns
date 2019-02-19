485a3774cfd13e486b822759da4b98ad67d899a5
Reinis Ozolins <reinisozolins@mail.com>
2019-02-19 12:50:55 +0200

    Add balking and adapter patterns

--- src/main/java/balking/App.java
+++ src/main/java/balking/App.java
@@ -8,11 +8,13 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+
 /**
  * In Balking Design Pattern if an object’s method is invoked when it is in an inappropriate state,
  * then the method will return without doing anything. Objects that use this pattern are generally only in a
  * state that is prone to balking temporarily but for an unknown amount of time
  *
+ * 
  * Imagine situation you have TV remote control system. In beginning status is "Mute" but when you push " + " button  its made
  * already max volume on your TV sound device. System inform you about it. When you push " + " button "i" times TV turn off.
  */
